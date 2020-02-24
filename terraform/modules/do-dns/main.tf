7e746418e8ed0ac8485d6a986218d05082a573d3
Sorin Lacriteanu <lacritea@adobe.com>
2020-02-24 09:22:52 +0200

    add lacriteanu.ro and www.lacriteanu.ro records

--- terraform/modules/do-dns/main.tf
+++ terraform/modules/do-dns/main.tf
@@ -3,6 +3,45 @@ variable "name" {
   description = "Name of the domain to be created"
 }
 
-resource "digitalocean_domain" "default" {
+# create  domain
+resource digitalocean_domain "default" {
   name       = var.name
+}
+
+locals {
+  domain_name = digitalocean_domain.default.name
+}
+
+output "domain_name" {
+  value = local.domain_name
+}
+
+# Add a record to the domain
+resource "digitalocean_record" "root" {
+  domain = digitalocean_domain.default.name
+  type   = "A"
+  name   = "@"
+  value  = "34.89.255.252"
+}
+
+# Output the FQDN for the record
+output "fqdn_root" {
+  value = digitalocean_record.root.fqdn
+}
+
+locals {
+  www_name = "www"
+}
+
+# Add a record to the domain
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.default.name
+  type   = "CNAME"
+  name   = "${local.www_name}."
+  value  = "${digitalocean_domain.default.name}."
+}
+
+# Output the FQDN for the record
+output "fqdn_www" {
+  value = digitalocean_record.www.fqdn
 }
